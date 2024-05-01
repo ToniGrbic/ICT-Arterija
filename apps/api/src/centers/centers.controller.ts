cb5cbf593d4555478a203abfb4462df3bcf5781d
rokoperisic <roko.perisic@gmail.com>
2024-05-01 21:07:32 +0200

    Added centers enpoints services

--- apps/api/src/centers/centers.controller.ts
+++ apps/api/src/centers/centers.controller.ts
@@ -1,33 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CentersService } from './centers.service';
 import { CreateCenterDto } from './dto/create-center.dto';
 import { UpdateCenterDto } from './dto/update-center.dto';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { centerEntity } from './entities/center.entity';
+import { AdminAuthGuard } from 'src/users/guards/admin-auth.guard';
 
 @Controller('centers')
+@ApiTags('Centers')
 export class CentersController {
   constructor(private readonly centersService: CentersService) {}
 
   @Post()
+  @UseGuards(AdminAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: centerEntity })
   create(@Body() createCenterDto: CreateCenterDto) {
     return this.centersService.create(createCenterDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: centerEntity })
   findAll() {
     return this.centersService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: centerEntity })
   findOne(@Param('id') id: string) {
     return this.centersService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(AdminAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: centerEntity })
   update(@Param('id') id: string, @Body() updateCenterDto: UpdateCenterDto) {
     return this.centersService.update(+id, updateCenterDto);
   }
 
   @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @ApiBearerAuth()
   remove(@Param('id') id: string) {
     return this.centersService.remove(+id);
   }
