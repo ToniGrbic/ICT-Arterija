12b976c4e93b5603ffdb173d8ddc794e0b3e9ae4
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-04 15:37:44 +0200

    findByEvent, findByUser and auth guards

--- apps/api/src/participants/participants.controller.ts
+++ apps/api/src/participants/participants.controller.ts
@@ -6,12 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { ParticipantsService } from './participants.service';
 import { CreateParticipantDto } from './dto/create-participant.dto';
 import { UpdateParticipantDto } from './dto/update-participant.dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { participantEntity } from './entities/participant.entity';
+import { AdminAuthGuard } from 'src/users/guards/admin-auth.guard';
 
 @Controller('participants')
 @ApiTags('Participants')
@@ -19,34 +23,51 @@ export class ParticipantsController {
   constructor(private readonly participantsService: ParticipantsService) {}
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(AdminAuthGuard)
   @ApiOkResponse({ type: participantEntity })
   create(@Body() createParticipantDto: CreateParticipantDto) {
     return this.participantsService.create(createParticipantDto);
   }
 
   @Get()
+  @ApiBearerAuth()
+  @UseGuards(AdminAuthGuard)
   @ApiOkResponse({ type: participantEntity })
   findAll() {
     return this.participantsService.findAll();
   }
 
   @Get(':id')
+  @ApiBearerAuth()
+  @UseGuards(AdminAuthGuard)
   @ApiOkResponse({ type: participantEntity })
-  findOne(@Param('id') id: string) {
-    return this.participantsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.participantsService.findOne(id);
   }
 
+  @Get('event/:id')
+  @ApiOkResponse({ type: participantEntity })
+  findByEvent(@Param('id', ParseIntPipe) id: number) {
+    return this.participantsService.findByEvent(id);
+  }
+
+  @Get('user')
+  @ApiOkResponse({ type: participantEntity })
+  findByUser(@Req() { user }) {
+    return this.participantsService.findByUser(user.id);
+  }
   @Patch(':id')
   @ApiOkResponse({ type: participantEntity })
   update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateParticipantDto: UpdateParticipantDto,
   ) {
-    return this.participantsService.update(+id, updateParticipantDto);
+    return this.participantsService.update(id, updateParticipantDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.participantsService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.participantsService.remove(id);
   }
 }
