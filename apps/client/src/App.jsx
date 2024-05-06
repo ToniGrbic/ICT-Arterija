9a7929e6a04ee7c4a08539913ffd4cf13d1514bc
rokoperisic <roko.perisic@gmail.com>
2024-05-07 01:47:47 +0200

    Added login page and api login request

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<>Home</>} />
-          <Route path="/login" element={<>Login</>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
