25fdbaaf76a658c0033778cc83d6d41c57184031
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-08 02:07:35 +0200

    outlet context for page name

--- apps/client/src/pages/Home/Home.jsx
+++ apps/client/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AutoPlay from "../../components/Sliders/AutoPlay/AutoPlay";
 import StatsBar from "../../components/StatsBar/StatsBar";
 import styles from "./index.module.css";
@@ -6,8 +6,14 @@ import ScheduleReminder from "../../components/ScheduleReminder/ScheduleReminder
 import EventsPreview from "../../components/Events/EventsPreview/EventsPreview";
 import ArrowRight from "../../icons/ArrowRight/ArrowRight";
 import BlogsPreview from "../../components/Blogs/BlogsPreview/BlogsPreview";
+import { useOutletContext } from "react-router-dom";
 
 const Home = () => {
+  const [_, setPageName] = useOutletContext();
+  useEffect(() => {
+    setPageName("Početna");
+  }, []);
+
   return (
     <>
       <AutoPlay />
