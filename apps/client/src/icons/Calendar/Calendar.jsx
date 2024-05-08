4dd4c2ca2cd97adde51edab8bb9cdaee53c986e5
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-08 02:11:15 +0200

    calendar icon

---
+++ apps/client/src/icons/Calendar/Calendar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import CalendarSvg from "../../assets/Calendar.svg";
+const Calendar = () => {
+  return <img src={CalendarSvg} alt="calendar" />;
+};
+
+export default Calendar;
