a09bcc0a58e5655719370b3dd28b07113ae80e52
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-08 02:08:00 +0200

    added routes for events and event/:id

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -2,20 +2,27 @@ import Home from "./pages/Home/Home";
 import MainLayout from "./components/Layouts/MainLayout/MainLayout";
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { EventsProvider } from "./providers/EventsProvider";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Events from "./pages/Events/Events";
+import Event from "./pages/Event/Event";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <EventsProvider>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/events/:id" element={<Event />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </EventsProvider>
       </BrowserRouter>
     </>
   );
