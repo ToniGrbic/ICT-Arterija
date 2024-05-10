5e9368605c37d4ee183e184203a9fd872dcdfd79
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 21:16:24 +0200

    number of saved lives for user

--- apps/client/src/components/StatsBar/StatsBar.jsx
+++ apps/client/src/components/StatsBar/StatsBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./index.module.css";
+import Cookies from "universal-cookie";
 
 const StatsBar = () => {
+  const cookies = new Cookies(null, { path: "/" });
+  const user = cookies.get("user");
+
   return (
     <div className={styles["statsbar-container"]}>
       <div className={styles["left-bar"]}></div>
-      <h2>Spasili ste 3 života</h2>
+      <h2>Spasili ste {Math.round(user.points / 100)} života</h2>
     </div>
   );
 };
