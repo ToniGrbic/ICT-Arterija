2775492aa1471ac0ca0e6ba30c629bc00f36c8e9
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 21:12:31 +0200

    added api property amd default values

--- apps/api/src/participants/entities/participant.entity.ts
+++ apps/api/src/participants/entities/participant.entity.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class participantEntity {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   user_id: number;
+
+  @ApiProperty()
   event_id: number;
+
+  @ApiProperty()
   is_working: boolean;
+
+  @ApiProperty()
   is_valid: boolean;
+
+  @ApiProperty()
   is_finished: boolean;
 }
