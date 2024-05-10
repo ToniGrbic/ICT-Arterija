c40026b7730a5143275918ff9910a0d64d9edb0d
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 02:23:26 +0200

    moved to another folder

--- apps/client/src/components/Popups/LogOffPopup/LogOffPopup.jsx
+++ apps/client/src/components/Popups/LogOffPopup/LogOffPopup.jsx
@@ -1,14 +1,14 @@
-import classses from "./index.module.css";
+import classes from "./index.module.css";
 
 // eslint-disable-next-line react/prop-types
 export default function LogOffPopup({ onConfirm, onCancel }) {
   return (
-    <div className={classses.logOffPopupContainer}>
+    <div className={classes.logOffPopupContainer}>
       <h2>Jeste li sigurni da se želite odjaviti?</h2>
-      <button className={classses.noButton} onClick={onCancel}>
+      <button className={classes.noButton} onClick={onCancel}>
         Ne
       </button>
-      <button className={classses.yesButton} onClick={onConfirm}>
+      <button className={classes.yesButton} onClick={onConfirm}>
         Da
       </button>
     </div>
