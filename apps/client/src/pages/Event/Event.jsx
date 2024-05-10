2c25a98b108e316ae52b8b6d1be77b2735582863
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 21:15:01 +0200

    event reservation

--- apps/client/src/pages/Event/Event.jsx
+++ apps/client/src/pages/Event/Event.jsx
@@ -1,17 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./index.module.css";
 import { useLocation } from "react-router-dom";
 import Calendar from "../../icons/Calendar/Calendar";
 import Clock from "../../icons/Clock/Clock";
 import Location from "../../icons/Location/Location";
+import ConfirmPopup from "../../components/Popups/ConfirmPopup/ConfirmPopup";
+import Cookies from "universal-cookie";
 
 const Event = () => {
+  const [showConfirmPopup, setShowConfirmPopup] = useState(false);
+  const [isJoined, setIsJoined] = useState(false);
   const { state: event } = useLocation();
+
   const [date, time] = event.date.split("T");
   const timeFormatted = time.slice(0, 5);
 
+  const cookies = new Cookies();
+  const user = cookies.get("user");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/participants/user`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const userParticipant = await res.json();
+
+        if (userParticipant.length === 0) return;
+
+        const isUserJoined = userParticipant.some(
+          (participant) => participant.event_id === event.id
+        );
+
+        setIsJoined(isUserJoined);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  const onConfirm = async () => {
+    try {
+      await fetch("http://localhost:3000/api/participants", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          event_id: event.id,
+          user_id: user.id,
+          is_working: true,
+          is_valid: true,
+          is_finished: false,
+        }),
+      });
+      setShowConfirmPopup(false);
+      setIsJoined(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles["event-wrapper"]}>
+      {showConfirmPopup && (
+        <ConfirmPopup
+          title="Jeste li sigurni da se želite prijaviti na ovaj događaj?"
+          onCancel={() => setShowConfirmPopup(false)}
+          onConfirm={() => onConfirm()}
+        />
+      )}
       <div className={styles["event-img-container"]}>
         <h3>{event.location}</h3>
         <img src={event?.image} />
@@ -41,7 +102,13 @@ const Event = () => {
           <p>{event.address}</p>
         </div>
       </div>
-      <button className={styles["event-join-button"]}>Donirajte i Vi</button>
+      <button
+        className={styles["event-join-button"]}
+        onClick={() => setShowConfirmPopup(true)}
+        disabled={isJoined || !user}
+      >
+        {isJoined ? "Prijavljeni ste" : "Donirajte i Vi"}
+      </button>
     </div>
   );
 };
