3081a55319ce04d00f4f1e53782e24a5f50220d7
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 21:13:26 +0200

    refactored from  LogOfPopup to general confirm popup

--- apps/client/src/components/Popups/ConfirmPopup/ConfirmPopup.jsx
+++ apps/client/src/components/Popups/ConfirmPopup/ConfirmPopup.jsx
@@ -1,10 +1,10 @@
 import classes from "./index.module.css";
 
 // eslint-disable-next-line react/prop-types
-export default function LogOffPopup({ onConfirm, onCancel }) {
+export default function ConfirmPopup({ onConfirm, onCancel, title }) {
   return (
     <div className={classes.logOffPopupContainer}>
-      <h2>Jeste li sigurni da se želite odjaviti?</h2>
+      <h2>{title}</h2>
       <button className={classes.noButton} onClick={onCancel}>
         Ne
       </button>
