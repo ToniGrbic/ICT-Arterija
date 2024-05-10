465c5322ef9e01d9bfc50b29f52e63c6d988f4d5
ToniGrbic <tonigrbic.5@gmail.com>
2024-05-10 21:13:41 +0200

    reward popup

--- apps/client/src/components/Popups/RewardsPopup/RewardsPopup.jsx
+++ apps/client/src/components/Popups/RewardsPopup/RewardsPopup.jsx
@@ -1,49 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
+import Cookies from "universal-cookie";
 
-const RewardsPopup = ({ reward, userPoints, showPopup }) => {
+const RewardsPopup = ({
+  reward,
+  userPoints,
+  setUserPoints,
+  showPopup,
+  setFilteredRewards,
+}) => {
+  const cookies = new Cookies();
+  const user = cookies.get("user");
+  const [isClaimed, setIsClaimed] = useState(false);
+
+  const claimReward = async (reward_id) => {
+    try {
+      const resRewards = await fetch(`api/user-rewards`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          reward_id,
+          user_id: user.id,
+          created_at: new Date().toISOString(),
+        }),
+      });
+      if (!resRewards.ok) throw new Error("Failed to claim reward");
+
+      const resUser = await fetch(`api/users/${user.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          points: user.points - reward.required_points,
+        }),
+      });
+      if (!resUser.ok) throw new Error("Failed to update user points");
+
+      setIsClaimed(true);
+      setUserPoints(user.points - reward.required_points);
+      setFilteredRewards((prev) =>
+        prev.filter((filteredReward) => filteredReward.id !== reward_id)
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles["rewards-popup-wrapper"]}>
       <div className={styles["rewards-popup-container"]}>
-        <h2>{reward.title}</h2>
-        <p>
-          U najljepšem gradu na svijetu uživajte uz napoletanske okuse i pogled
-          koji osvaja dah. U ručku za dvije osobe uživajte s osobom koju želite
-          šarmirati dobrom atmosferom i ukusnom hranom na najatraktivnijoj
-          lokaciji u gradu Splitu.
-        </p>
-        {userPoints ? (
-          <div className={styles["rewards-popup-claim"]}>
-            <button
-              className={`${styles["rewards-popup-claim-btn"]} ${styles["btn-active"]}`}
-              disabled={userPoints < reward.required_points}
-            >
-              Preuzmite nagradu
-            </button>
-            <p
-              style={
-                userPoints < reward.required_points
-                  ? { color: "rgba(18, 27, 40, 0.20)" }
-                  : { color: "" }
-              }
-            >
-              {reward.required_points} bodova
-            </p>
-          </div>
+        {isClaimed ? (
+          <h2>Nagrada je uspješno preuzeta!</h2>
         ) : (
-          <div className={styles["rewards-popup-claim"]}>
-            <button
-              className={`${styles["rewards-popup-claim-btn"]}`}
-              disabled={true}
-            >
-              Preuzmite nagradu
-            </button>
-            <p style={{ color: "rgba(18, 27, 40, 0.20)" }}>
-              {reward.required_points} bodova
-            </p>
-          </div>
+          <>
+            <h2>{reward.name}</h2>
+            <p>{reward.description}</p>
+            {userPoints ? (
+              <div className={styles["rewards-popup-claim"]}>
+                <button
+                  className={`${styles["rewards-popup-claim-btn"]} ${styles["btn-active"]}`}
+                  disabled={userPoints < reward.required_points}
+                  onClick={() => claimReward(reward.id)}
+                >
+                  Preuzmite nagradu
+                </button>
+                <p
+                  style={
+                    userPoints < reward.required_points
+                      ? { color: "rgba(18, 27, 40, 0.20)" }
+                      : { color: "" }
+                  }
+                >
+                  {reward.required_points} bodova
+                </p>
+              </div>
+            ) : (
+              <div className={styles["rewards-popup-claim"]}>
+                <button
+                  className={`${styles["rewards-popup-claim-btn"]}`}
+                  disabled={true}
+                >
+                  Preuzmite nagradu
+                </button>
+                <p style={{ color: "rgba(18, 27, 40, 0.20)" }}>
+                  {reward.required_points} bodova
+                </p>
+              </div>
+            )}
+          </>
         )}
-
         <button
           onClick={() => {
             showPopup(false);
